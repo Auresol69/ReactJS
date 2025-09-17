a0b2a42210c9846690ad4fc12e6a767a6ea25db1
Thai Quoc Bao <quocbao962005@gmail.com>
2025-09-18 02:18:22 +0700

    stop watch app

--- To_Do_List/src/App.jsx
+++ To_Do_List/src/App.jsx
@@ -1,12 +1,15 @@
 import ToDoList from "./ToDoList"
 import Digital_Clock from "./Digital_Clock"
+import Stop_Watch_App from "./Stop_Watch_App"
 
 function App() {
 
   return (<>
-      <ToDoList />
-      <hr />
-      <Digital_Clock />
+    <ToDoList />
+    <hr />
+    <Digital_Clock />
+    <hr />
+    <Stop_Watch_App />
   </>
   )
 }
